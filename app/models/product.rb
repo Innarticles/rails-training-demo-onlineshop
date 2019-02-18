8f57b72a65c2ebb7751ad5740391bf8218238525
Innocent Udeogu <innocentudeh@gmail.com>
2019-02-18 13:19:47 +0100

    added assets

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  category    :string
+#  created_at  :datetime         not null
+#  description :text
+#  id          :bigint(8)        not null, primary key
+#  price       :float
+#  sale_price  :float
+#  title       :string
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
 end
